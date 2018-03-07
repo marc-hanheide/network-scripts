b3a1171b48417a03eb0f0592c9a56a622138921e
Marc Hanheide <marc@hanheide.de>
2018-03-07 09:12:03 +0000

    made nmap optional and work on Darwin

--- iptracker.sh
+++ iptracker.sh
@@ -21,11 +21,22 @@ while getopts "hvu:" opt; do
   esac
 done
 
-default_iface=$(awk '$2 == 00000000 { print $1 }' /proc/net/route)
-default_ip=`ip addr show dev "$default_iface" | grep "inet " | sed 's@ *inet \([0-9\.]*\).*@\1@'`
+system="`uname`"
+if [ "$system" = "Darwin" ]; then
+  default_iface=`route get default | grep interface| sed 's/ *interface: \(.*\)$/\1/'`
+  default_ip=`ifconfig en1 | grep "inet " | sed 's@^.*inet \([0-9\.]*\).*@\1@'`
+else
+  default_iface=$(awk '$2 == 00000000 { print $1 }' /proc/net/route)
+  default_ip=`ip addr show dev "$default_iface" | grep "inet " | sed 's@ *inet \([0-9\.]*\).*@\1@'`
+fi
 default_hostname=`hostname`
 update_date="`date| sed 's/ /%20/g'`"
-ports=`nmap $default_ip | grep " open" | cut -f1 -d"/" | tr "\n" "," || echo "nmap%20 not%20 found"` 2>&1 > /dev/null
+
+if which nmap > /dev/null 2>&1; then
+  ports=`nmap $default_ip | grep " open" | cut -f1 -d"/" | tr "\n" "," || echo "nmap%20 not%20 found"` 2>&1 > /dev/null
+else
+  ports="no%20nmap"
+fi
 
 params="name=$default_hostname&ip=$default_ip&comment=iface%20$default_iface%20ports:%20$ports&updated=$update_date"
 fullurl="$tracker_url?$params"
@@ -34,10 +45,12 @@ fullurl="$tracker_url?$params"
 QUIET="-s"
 
 if [ "$VERBOSE" ]; then
+  echo "system:        $system"
 	echo "default_iface: $default_iface"
 	echo "default_ip:    $default_ip"
 	echo "tracker_url:   $tracker_url"
 	echo "fullurl:       $fullurl"
+  echo "ports:         $ports"
 	QUIET=""
 fi
 
