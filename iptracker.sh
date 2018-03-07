72535691285ff893a3978a4948fca6df54087806
Marc Hanheide <marc@hanheide.de>
2018-03-07 09:19:33 +0000

    public_ip

--- iptracker.sh
+++ iptracker.sh
@@ -33,12 +33,20 @@ default_hostname=`hostname`
 update_date="`date| sed 's/ /%20/g'`"
 
 if which nmap > /dev/null 2>&1; then
-  ports=`nmap $default_ip | grep " open" | cut -f1 -d"/" | tr "\n" "," || echo "nmap%20 not%20 found"` 2>&1 > /dev/null
+  ports=`nmap $default_ip | grep " open" | cut -f1 -d"/" | tr "\n" ","`
 else
   ports="no%20nmap"
 fi
 
-params="name=$default_hostname&ip=$default_ip&comment=iface%20$default_iface%20ports:%20$ports&updated=$update_date"
+if which dig > /dev/null 2>&1; then
+  public_ip=`dig TXT +short o-o.myaddr.l.google.com @ns1.google.com | awk -F'"' '{ print $2}'`
+else
+  public_ip="no%20dig"
+fi
+
+
+
+params="name=$default_hostname&ip=$default_ip&comment=iface%20$default_iface%20ports:%20$ports&updated=$update_date&public_ip=$public_ip"
 fullurl="$tracker_url?$params"
 
 
@@ -48,6 +56,7 @@ if [ "$VERBOSE" ]; then
   echo "system:        $system"
 	echo "default_iface: $default_iface"
 	echo "default_ip:    $default_ip"
+  echo "public_ ip:    $public_ip"
 	echo "tracker_url:   $tracker_url"
 	echo "fullurl:       $fullurl"
   echo "ports:         $ports"
